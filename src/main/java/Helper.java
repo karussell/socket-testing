278856a8357e8df45497eb435e70fc63cc201f94
Peter <graphhopper@gmx.de>
2020-12-01 17:30:37 +0100

    bubble sort

--- src/main/java/Helper.java
+++ src/main/java/Helper.java
@@ -1,8 +1,26 @@
+import java.util.List;
+
 public class Helper {
     String str;
 
     public Helper(String str) {
         this.str = str;
+    }
+
+    public static void sort(List<Integer> list) {
+        boolean unsorted = false;
+
+        while(!unsorted) {
+            unsorted = true;
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i - 1) > list.get(i)) {
+                    Integer tmp = list.get(i - 1);
+                    list.set(i - 1, list.get(i));
+                    list.set(i, tmp);
+                    unsorted = false;
+                }
+            }
+        }
     }
 
     public int count(String substring) {
--- src/test/java/HelperTest.java
+++ src/test/java/HelperTest.java
@@ -1,30 +1,49 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HelperTest {
 
     @Test
-    public void count() {
-        Helper objectToTest = new Helper("sss");
-        int result = objectToTest.count("s");
-        assertEquals(3, result);
-
-        result = objectToTest.count("a");
-        assertEquals(0, result);
+    public void sort() {
+        List<Integer> list = Arrays.asList(6, 8, 2, 4, 4, 7, 2, 6, 9);
+        Helper.sort(list);
+        assertEquals(Arrays.asList(2, 2, 4, 4, 6, 6, 7, 8, 9), list);
     }
 
     @Test
-    public void complexCount() {
-        Helper objectToTest = new Helper("blaskas");
-        int result = objectToTest.count("as");
-        assertEquals(2, result);
+    public void perfTest() {
+        List<Integer> list = createRandomList(1000);
+
+        // warm up
+        long sum = 0;
+        for (int i = 0; i < 300; i++) {
+            Helper.sort(new ArrayList<>(list));
+            sum += list.get(0);
+        }
+
+        // speed test
+        long start = System.nanoTime();
+        int COUNT = 1000;
+        sum = 0;
+        for (int i = 0; i < COUNT; i++) {
+            Helper.sort(new ArrayList<>(list));
+            sum += list.get(0);
+        }
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1e9 / COUNT);
+        System.out.println(sum);
     }
 
-    @Test
-    public void emptyNull() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Helper("").count(null));
+    List<Integer> createRandomList(int size) {
+        Random rand = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(rand.nextInt(size / 2));
+        }
+        return list;
     }
 }
